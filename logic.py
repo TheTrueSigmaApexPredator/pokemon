04d5174010fe24f4bd0a7fa3a18946dbc8743a3d
ApexPredator67 <pavelvelikiy.10.18@gmail.com>
2025-12-20 00:34:04 +0300

    this

--- logic.py
+++ logic.py
@@ -6,12 +6,15 @@ class Pokemon:
     # Инициализация объекта (конструктор)
     def __init__(self, pokemon_trainer):
 
-        self.pokemon_trainer = pokemon_trainer   
+        self.pokemon_trainer = pokemon_trainer 
+        self.pokemon_number = randint(1,1000)  
         self.weight = self.get_weight()
         self.name = self.get_name()
-        self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.hunger = randint(20, 70) 
+        self.power = randint(30, 90)
+        self.hp = randint(200, 400)
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -39,6 +42,52 @@ class Pokemon:
         else:
             return "Pikachu"
         
+
+
+
+
+        
+
+
+    def feed(self, food_type="обычная"):
+        if self.hunger <= 0:
+            return f"{self.name} не голоден!"
+        
+        food_effects = {
+            "обычная еда": {"hunger": -20},
+            "ягода": {"hunger": -30},
+            "эликсир": {"hunger": -50},
+            "торт": {"hunger": -40}
+        }
+
+        
+        
+        effect = food_effects.get(food_type, food_effects["обычная еда"])
+        self.hunger = max(0, self.hunger + effect["hunger"])
+        response = f"{self.name} покормлен({food_type})! Сытость: {100 - self.hunger}%"
+
+        return response
+    
+    
+    def hunger_status(self):
+        if self.hunger <= 10:
+            return "сытый"
+        elif self.hunger <= 30:
+            return "немного голоден"
+        elif self.hunger <= 50:
+            return "голоден"
+        elif self.hunger <= 70:
+            return "очень голоден"
+        else:
+            return "умирает от голода!"
+        
+
+
+
+
+
+
+
     def get_weight(self):
         url = f"https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}"
         response = requests.get(url)
@@ -47,12 +96,14 @@ class Pokemon:
             return (data["weight"])
         else:
             return "60"
+        
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покемона: {self.name}"
-    def show_weight(self):
-        return f"Вес твоего покемона: {self.weight}"
+        return (f"Имя твоего покемона: {self.name}\n"
+                f"Вес твоего покемона: {self.weight}\n"
+                f"Сила твоего покемона: {self.power}\n"
+                f"Здоровье твоего покемона: {self.hp}")
     
     # тут имя изменяется ⬇⬇⬇⬇
 
@@ -63,6 +114,35 @@ class Pokemon:
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+    
 
 
 
+
+
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard):
+            chance = randint(1, 5)
+            if chance == 2:
+                return 'Покемон-волшебник применил щит в сражении'
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+
+
+class Wizard(Pokemon):
+    def info(self):
+        return ' У тебя покемон-волшебник\n\n' + super().info()
+    
+class Fighter(Pokemon):
+    def info(self):
+        return ' У тебя покемон-боец\n\n' + super().info()
+    def attack(self, enemy):
+        super_power = randint(5, 15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f'\nБоец примеил супер атаку силой {super_power}'
--- main.py
+++ main.py
@@ -1,20 +1,57 @@
-import telebot 
+import telebot
+from random import randint
 from config import token
-
-from logic import Pokemon
+from logic import Pokemon, Wizard, Fighter
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['go'])
-def go(message):
+def start(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
-        bot.send_message(message.chat.id, pokemon.info())
-        bot.send_message(message.chat.id, pokemon.show_weight())
+        chance = randint(1, 3)
+        if chance == 1:
+            pokemon = Pokemon(message.from_user.username)
+        elif chance == 2:
+            pokemon = Wizard(message.from_user.username)
+        elif chance == 3:
+            pokemon = Fighter(message.from_user.username)
+        
+        bot.send_message(message.chat.id, f"Поздравляем! Ты создал покемона!\n\n{pokemon.info()}")
         bot.send_photo(message.chat.id, pokemon.show_img())
+        
+        # Проверка типа покемона
+        if chance == 2:  # Wizard
+            type_msg = "Твой покемон - Волшебник! Может использовать щит в бою."
+        elif chance == 3:  # Fighter
+            type_msg = "Твой покемон - Боец! Имеет шанс нанести критический удар."
+        else:  # обычный
+            type_msg = "Твой покемон - обычный тип."
+            
+        bot.send_message(message.chat.id, type_msg)
     else:
-        bot.reply_to(message, "Ты уже создал себе покемона")
+        bot.reply_to(message, "Ты уже создал себе покемона! Используй /attack чтобы атаковать другого человека.")
 
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        attacker_username = message.from_user.username
+        defender_username = message.reply_to_message.from_user.username
+        
+        if (defender_username in Pokemon.pokemons.keys() and 
+            attacker_username in Pokemon.pokemons.keys()):
+            
+            enemy = Pokemon.pokemons[defender_username]
+            pok = Pokemon.pokemons[attacker_username]
+            
+            res = pok.attack(enemy)
+            
+            bot.send_message(message.chat.id, res)
+            
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+            
+    else:
+        bot.send_message(message.chat.id, 
+                         "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
 
 bot.infinity_polling(none_stop=True)
-
